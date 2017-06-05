e1e75acef33ece0c8a9d05a1ba4d3ef049cceed8
Tzu-Yin Hong <lili668668@gmail.com>
2017-06-05 14:21:12 +0800

    learn how to show graph in tensorflowboard

---
+++ demotfb.sh
@@ -0,0 +1,2 @@
+python trytfb.py
+tensorboard --logdir='logs/'
---
+++ trytfb.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import numpy as np
+
+# create input data and train data
+xd = np.random.rand(100).astype(np.float32)
+yd = xd * 0.1 + 0.3
+
+with tf.name_scope('inputs'):
+    xs = tf.placeholder(tf.float32, name='x_input')
+    ys = tf.placeholder(tf.float32, name='y_input')
+
+# make a conputational graph
+with tf.name_scope('layer'):
+    with tf.name_scope('wieghts'):
+        Weights = tf.Variable(tf.random_uniform([1], -1.0 , 1.0), name="W")
+    with tf.name_scope('biases'):
+        biases = tf.Variable(tf.zeros([1]), name="b")
+
+    with tf.name_scope('Wx_add_b'):
+        y = Weights * xs + biases
+
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.square(y-ys))
+with tf.name_scope('train'):
+    optimizer = tf.train.GradientDescentOptimizer(0.5)
+    train = optimizer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+# run the tensorflow
+sess = tf.Session()
+writer = tf.summary.FileWriter("logs/", sess.graph)
+sess.run(init)
+
+for step in range(2017):
+    sess.run(train, feed_dict={xs: xd, ys: yd})
+    # if step % 20 == 0:
+        # print step, sess.run(Weights), sess.run(biases)
+feed_dict = {xs: [10, 20, 40]}
+prediction = sess.run(y, feed_dict)
+print prediction
+
