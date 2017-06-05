b4b0a2cf2988881134cba5a80f0a2a2d79332f19
Tzu-Yin Hong <lili668668@gmail.com>
2017-06-05 14:31:15 +0800

    tmp

--- trytfb.py
+++ trytfb.py
@@ -13,14 +13,17 @@ with tf.name_scope('inputs'):
 with tf.name_scope('layer'):
     with tf.name_scope('wieghts'):
         Weights = tf.Variable(tf.random_uniform([1], -1.0 , 1.0), name="W")
+        tf.summary.histogram('weights', Weights)
     with tf.name_scope('biases'):
         biases = tf.Variable(tf.zeros([1]), name="b")
+        tf.summary.histogram('biases', biases)
 
     with tf.name_scope('Wx_add_b'):
         y = Weights * xs + biases
 
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.square(y-ys))
+    tf.summary.scalar('loss', loss)
 with tf.name_scope('train'):
     optimizer = tf.train.GradientDescentOptimizer(0.5)
     train = optimizer.minimize(loss)
@@ -29,13 +32,15 @@ init = tf.initialize_all_variables()
 
 # run the tensorflow
 sess = tf.Session()
+merged = tf.summary.merge_all()
 writer = tf.summary.FileWriter("logs/", sess.graph)
 sess.run(init)
 
 for step in range(2017):
     sess.run(train, feed_dict={xs: xd, ys: yd})
-    # if step % 20 == 0:
-        # print step, sess.run(Weights), sess.run(biases)
+    if step % 20 == 0:
+        result = sess.run(merged, feed_dict={xs: xd, ys: yd})
+        print step, sess.run(Weights), sess.run(biases)
 feed_dict = {xs: [10, 20, 40]}
 prediction = sess.run(y, feed_dict)
 print prediction
