9772f232d8940d2939117d4d33882c66c14c6c20
Tzu-Yin Hong <lili668668@gmail.com>
2017-06-07 11:41:49 +0800

    try to save module

--- demomlsumit.sh
+++ demomlsumit.sh
@@ -1,5 +1,5 @@
-gcloud ml-engine jobs submit training demo2 \
-    --job-dir gs://learn_talk/demo2 \
+gcloud ml-engine jobs submit training demo3 \
+    --job-dir gs://learn_talk/demo3 \
     --runtime-version 1.0 \
     --module-name tfb.trytfb \
     --package-path tfb/ \
